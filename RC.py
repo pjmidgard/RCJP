b5031c7c3638587b7313997709a1397f7c647466
Java Software <pj.midgard@yahoo.com>
2022-06-26 13:46:58 +0100

    Update RC.py

--- RC.py
+++ RC.py
@@ -645,7 +645,7 @@ class compression:
                                                                                 if block_compression2==2:
                                                                                     block_compression2=3
                                             
-                                                                            sub_info3=str_find_tree_maches[find_matches1_number2+4:find_matches1_number2+7]
+                                                                            sub_info3=str_find_tree_maches[find_matches1_number3+4:find_matches1_number3+7]
                                                                             
                                                                             find_matches_1=str_find_tree_maches.find("000000", find_matches1_number1+4, end)
                                                                             if find_matches_1=="-1":  
